a95698b4e351ac1748ed5d3dba2b11b7bd0b687c
Viktoria Antalikova <xantal03@vutbr.cz>
2022-04-11 11:50:35 +0200

    funkcia - read_data()

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,10 +12,22 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
-    file_path = os.path.join(cwd_path, file_name)
+    file_path = os.path.join(cwd_path, file_name) # z os ziskal cwd, do neho ulozil aktualny pracovny adresar
+
+    if field not in {"unordered_numbers","ordered_numbers","dna_sequence"}:
+        return None
+
+    with open(file_path, 'r') as subor:
+        slovnicek = json.load(subor)
+
+    return slovnicek[field]
+
 
 
 def main():
+    file_name = 'sequential.json'
+    sequential_data = read_data(file_name, 'unordered_numbers')
+    print(sequential_data)
     pass
 
 
